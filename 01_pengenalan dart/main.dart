3e8435740de9b280ae55fd19471deac2d100de31
nurdiansyahRM <nurdiansyahjr46@gmail.com>
2022-10-17 20:02:45 +0700

    pengenalan dart

---
+++ 01_pengenalan dart/main.dart
@@ -0,0 +1,14 @@
+main() {
+  String nama = 'Nurdiansyah RM';
+  String language = 'dart';
+  print('Hello $nama. Welcome to $language');
+  for (int i = 0; i < 5; i++) {
+    print('hallo dunia ${i + 1}');
+  }
+}
+/* dart merupakan bahasa yang statically typed
+  Type Inference
+{Ini merupakan comment multi line}
+*/
+
+// ini merupakan commment single line
