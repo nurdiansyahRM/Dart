8b0400bfd52fbc00a1e7b0c6d2ae4c096cf69d6d
nurdiansyahRM <nurdiansyahjr46@gmail.com>
2022-10-25 12:03:19 +0700

    modified part of 09_exceptions

--- 09_Exceptions/main.dart
+++ 09_Exceptions/main.dart
@@ -10,10 +10,30 @@ void main(){
   // }
 
   // 2. dengan try catch untuk mengurung kesalahan pada potongan kode
-  try{
+  /*try{
     var hasil = a ~/ b;
     print('$hasil');
   }catch(e){
     print('Exception Happend $e');
+  }*/
+  // 3. selain dengan menggunakan try catch yang memberikan satu parameter bisa juga menambahkan objek stack trace :
+  /*try{
+    var hasil = a ~/b;
+    print('$hasil');
+
+  }catch(e,s){
+    print('exception Happend : $e');
+    print('strack trace $s');
+  }*/
+  // 4. selain dengan adanya try, catch dan digabungkan lagi dengan on ada juga fitur pada menangkap kesalahan yaitu block
+  try{
+    var hasil = a ~/b;
+    print('$hasil');
+
+  }catch(e,s) {
+    print('exception Happend : $e');
+    print('strack trace $s');
+  }finally{
+    print('this is till excecuted');
   }
 }
