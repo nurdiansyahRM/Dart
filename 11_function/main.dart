b652fd9080ff879c4affd60fa6819d339d36845d
nurdiansyahRM <nurdiansyahjr46@gmail.com>
2022-10-26 10:29:49 +0700

    menambahkan folder 11 function

---
+++ 11_function/main.dart
@@ -0,0 +1,13 @@
+String inputnama(String nama){
+  print('Hallo $nama');
+  return nama;
+}
+double average(num num1, num num2){
+  return (num1 + num2) /2;
+}
+void main(){
+ var firstnumber = 7;
+ var secondNumber = 8;
+ print('Rata - Rata dari $firstnumber & $secondNumber adalah ${average(firstnumber, secondNumber)}');
+ inputnama('nurdiansyah');
+}
