cbe9d70ecfbf79b2d79bd0e5420879c27b5e1cc2
nurdiansyahRM <nurdiansyahjr46@gmail.com>
2022-10-30 19:20:51 +0700

    menambah seri baru manipulasi set union dan intersection 22

---
+++ 22_manipulasi set union & intersection/main.dart
@@ -0,0 +1,9 @@
+void main(){
+ var SetA = {1, 2, 4, 5};
+ var SetB = {1, 5, 7};
+
+ var union = SetA.union(SetB);
+ var intersection = SetA.intersection(SetB);
+ print(union);
+ print(intersection);
+}
