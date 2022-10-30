d451fb984d3353a420ae0e1bffb2d8631c397f72
nurdiansyahRM <nurdiansyahjr46@gmail.com>
2022-10-30 12:32:04 +0700

    menambahkan seri baru pada folder 20 dengan set collection

--- 19_manipulasi map/main.dart
+++ 19_manipulasi map/main.dart
@@ -10,4 +10,16 @@ void main(){
   print('2. menambahkan data pada List di awal atau diakhir');
   StringList.insert(0, 'programming zamannow');
   print(StringList);
+  // 3. memanipulasi edit data pada list
+  print('3. mengubah data pada list ');
+  print(StringList);
+  StringList[0] = 'Programming ZamanNow';
+  print(StringList);
+  // 4. memanipulasi dengan menghapus data pada list dengan keyword Remove
+ print('4. memanipulasi dengan menghapus data pada list');
+  //StringList.remove('WPU'); // remove ini menghapus data  mengacu pada nilai pada list
+  //StringList.removeAt(0); // remove ini menghapus data mengacu pada index pada list
+  //StringList.removeLast(); //remove ini menghapus data pada data terakhir
+  StringList.removeRange(0, 2); //remove ini menghapus data pada range index 0 sampai 1
+  print(StringList);
 }
